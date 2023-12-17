6a605271bd648cee3fb603c52ae3d23563b7fbb5
Umutcan Kocamış <66033176+ukhazards@users.noreply.github.com>
2023-12-17 17:27:57 +0300

    Create dev branch

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -4,6 +4,12 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+
+""""Dev initial commit"""
+
+
+
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
